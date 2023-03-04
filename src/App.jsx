69b6fcd2b519e11281a5e9c03d6df2d935a2cb68
WLaurent-Hub <laurentwu123@gmail.com>
2023-03-04 21:08:52 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Navbar from './components/Navbar';
 import Map from './components/Map';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>topto</h1>
+            <h1>test</h1>
         </div>
     );
 };
